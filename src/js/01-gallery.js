82b5396fd6cb3d94aafdf948ff8b8998adf1e584
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-09-24 16:22:35 +0300

    Revert "change js files"
    This reverts commit c9554d392d7765c4d6f637fb8b53928c0ced6205.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryRef = document.querySelector('.gallery');
-
-const markupFromArr = galleryItems.map((elem) => {
-    const {preview, original, description } = elem
-    return `<div class="gallery__item">
-    <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-  </div>`
-})
-const markupFromArrString = markupFromArr.join("")
-galleryRef.insertAdjacentHTML("afterbegin", markupFromArrString);
-
-
-const lightbox = new SimpleLightbox('.gallery a', { 
-    captionsData: 'alt',
-    captionPosition: 'top',
-    captionType: 'attr',
-    captionSelector: '.gallery__image',
-    captionDelay:	250,
-    showCounter: false,
-    
- });
+console.log(galleryItems);
